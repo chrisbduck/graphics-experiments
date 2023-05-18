09891daa6c2c633ec5ff01fe455524505b7f797e
chrisbduck <chrisbduck>
2023-05-18 10:09:07 -0700

    Made plasma effect much faster

--- PlasmaEntity.cpp
+++ PlasmaEntity.cpp
@@ -12,10 +12,42 @@
 
 using namespace std;
 
+namespace
+{
+	constexpr size_t c_lookupSize = 1 << 10;
+	constexpr size_t c_lookupSizeMask = c_lookupSize - 1;
+	constexpr float twoPi = static_cast<float>(numbers::pi) * 2.0f;
+	constexpr float c_sinCosToLookupScale = static_cast<float>(c_lookupSize) / twoPi;
+}
+
 PlasmaEntity::PlasmaEntity(int width, int height) :
-	SpriteEntity(width, height)
+	SpriteEntity(width, height),
+	m_sinLookup(c_lookupSize),
+	m_cosLookup(c_lookupSize)
 {
 	setTextureData();
+	constexpr float scale = twoPi / c_lookupSize;
+	for (size_t index = 0; index < c_lookupSize; ++index)
+	{
+		float value = static_cast<float>(index) * scale;
+		m_sinLookup[index] = sinf(value);
+		m_cosLookup[index] = cosf(value);
+	}
+}
+
+size_t PlasmaEntity::toLookupIndex(float value) const
+{
+	return static_cast<size_t>(value * c_sinCosToLookupScale) & c_lookupSizeMask;
+}
+
+float PlasmaEntity::fastSin(float value) const
+{
+	return m_sinLookup[toLookupIndex(value)];
+}
+
+float PlasmaEntity::fastCos(float value) const
+{
+	return m_cosLookup[toLookupIndex(value)];
 }
 
 void PlasmaEntity::update()
@@ -33,8 +65,9 @@ void PlasmaEntity::setTextureData()
 	// Generate a plasma
 	float invWidth = 1.0f / width;
 	float invHeight = 1.0f / height;
-	const float two_pi = static_cast<float>(numbers::pi) * 2.0f;
-	float timeSec = timeSinceEpochInSeconds();
+	const float timeSec = timeSinceEpochInSeconds();
+	const float timeTimesTwoPi = timeSec * twoPi;
+	const float quarterTime = timeSec * 0.25f;
 	unsigned index = 0;
 	for (unsigned y = 0; y < height; ++y)
 	{
@@ -42,15 +75,18 @@ void PlasmaEntity::setTextureData()
 		for (unsigned x = 0; x < width; ++x)
 		{
 			float xRatio = x * invWidth;
-			float val = (sinf(xRatio * 10.0f + cosf(yRatio * 20.0f))
-				+ sinf(yRatio * 5.0f + sinf(xRatio * 30.0f))
-				+ sinf((xRatio + yRatio) * 25.0f)
-				+ sinf(sqrtf(xRatio * xRatio + yRatio * yRatio)))
+			float val = (fastSin((xRatio + quarterTime) * 10.0f + fastCos(yRatio * 20.0f))
+				+ fastSin(yRatio * 5.0f + fastSin(xRatio * 30.0f))
+				+ fastSin((xRatio + yRatio) * 25.0f)
+				+ fastSin(sqrtf(xRatio * xRatio + yRatio * yRatio)))
 				* 0.25f + 0.5f;
-			float valR = val * 0.5f + sinf(timeSec * two_pi * 0.25f) * 0.5f;
-			unsigned uVal = static_cast<unsigned>(clamp(val, 0.0f, 1.0f) * 255.0f);
+			float valR = 0.5f * (val + fastSin(timeTimesTwoPi * (1.0f / 4.0f)));
+			float valG = 0.5f * (val + fastSin(timeTimesTwoPi * (1.0f / 7.0f)));
+			float valB = 0.5f * (val + fastSin(timeTimesTwoPi * (1.0f / 9.0f)));
 			unsigned uValR = static_cast<unsigned>(clamp(valR, 0.0f, 1.0f) * 255.0f);
-			auto color = 0xFF000000 | uValR << 16 | uVal << 8 | uVal;
+			unsigned uValG = static_cast<unsigned>(clamp(valG, 0.0f, 1.0f) * 255.0f);
+			unsigned uValB = static_cast<unsigned>(clamp(valB, 0.0f, 1.0f) * 255.0f);
+			auto color = 0xFF000000 | uValB << 16 | uValG << 8 | uValR;
 			pixels[index] = color;
 			++index;
 		}
--- PlasmaEntity.h
+++ PlasmaEntity.h
@@ -9,5 +9,12 @@ public:
 	virtual void update() override;
 private:
 	void setTextureData();
+	size_t toLookupIndex(float value) const;
+	float fastSin(float value) const;
+	float fastCos(float value) const;
+
+	// Using lookups instead of sinf/cosf gives ~10x speedup
+	std::vector<float> m_sinLookup;
+	std::vector<float> m_cosLookup;
 };
 
